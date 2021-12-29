4bc6fee44be7f3685970a506100d20b7856bff3b
Rajat Verma <rajat.verma78402@gmail.com>
2021-12-29 23:34:38 +0530

    More front end features

--- PeP-Coding/29_Dec_21/main.js
+++ PeP-Coding/29_Dec_21/main.js
@@ -1,38 +1,21 @@
 (function(){
-    let btn = document.querySelector("#myFirstButton");
-    let divContainer = document.querySelector("#container");
-    let myTemplates = document.querySelector("#myTemplates");
+    let btnAddFolder = document.querySelector("#btnAddFolder");
+    let divContainer = document.querySelector("#divContainer");
+    let pageTemplates = document.querySelector("#pageTemplates");
 
-    btn.addEventListener("click", function(){
-        let fname = prompt("enter a folder's name");
+    btnAddFolder.addEventListener("click", function(){
+        let fname = prompt("Folder name?");
         if(fname == null){
             return;
         }
 
-        let divFolderTemplate = myTemplates.content.querySelector(".folder");
+        let divFolderTemplate = pageTemplates.content.querySelector(".folder");
         let divFolder = document.importNode(divFolderTemplate, true);
 
-        divFolder.innerHTML = fname;
+        let divName = divFolder.querySelector("[purpose='name']");
+        divName.innerHTML = fname;
+
         divContainer.appendChild(divFolder);
-    })
 
+    });
 })();
-
-// (function(){
-//     let btn = document.querySelector("#myFirstButton");
-//     let divContainer = document.querySelector("#container");
-//     let myTemplates = document.querySelector("#myTemplates");
-
-//     btn.addEventListener("click", function(){
-//         let fname = prompt("Enter a folder's name");
-        
-//         let divFolderTemplate = myTemplates.content.querySelector(".folder");
-//         let divFolder = document.importNode(divFolderTemplate, true);
-
-//         divFolder.innerHTML = fname;
-//         divContainer.appendChild(divFolder);
-
-        
-//     })
-
-// })();
