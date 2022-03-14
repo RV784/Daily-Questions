1618de1946e1e073c40e03738df45dfcb30f38b5
Rajat Verma <rajat.verma78402@gmail.com>
2022-03-14 23:30:52 +0530

    Level 2 HashMap questions

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,16 +1,35 @@
 package OOPS;
+// import java.util.*;
+// import bank;
 
 class Shape{
-    String color;
+    // String color;
+    public void area(){
+
+    }
 }
 
 class Triangle extends Shape{
+    public void area(int l, int h){
+        System.out.println((1/2)*(l)*(h));
+    }
+}
 
+class Circle extends Shape{
+    public void area(int r){
+        System.out.println(3.13*r*r); 
+    }
+}
+
+class EuilateralTriangle extends Triangle{
+    public void area(int l, int h){
+        System.out.println((1/2)*(l)*(h));
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Triangle t1 = new Triangle();
-        t1.color = "red";
+
+        
     }
 }
